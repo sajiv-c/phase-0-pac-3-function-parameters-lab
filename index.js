61186186b3e12db9d7072c24cdb718e6979b524d
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-09-19 18:08:42 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name, language) {
+    if (language == null) {
+        return `Hi, my name is ${name} and I am learning to program in JavaScript.`;
+    }
+    else {
+        return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    }
+}
